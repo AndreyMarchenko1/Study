a967dd37f7a501f350a9697e5c45e69e347f94dd
AndreyMarchenko1 <andrusha192@vk.com>
2024-12-03 21:24:26 +0300

    Add files via upload

---
+++ ConsoleApp1/Program2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace DeliveryApp
+{
+    class Program2
+    {
+        partial class Items
+        {
+            public MyFoodType TypeOfGood { get; set; }
+        }
+    }
+}
